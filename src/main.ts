7c118f77dedade00fd4af8941d914324b665b421
lyshenshing <shing.ly@pathmazing.com>
2021-12-06 15:13:30 +0700

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Custom Project')
+    .setDescription('Custom Project Descriptions')
+    .setVersion('1.0.0')
+    .addTag('Custom Project API')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+  console.log('connected');
 }
 bootstrap();
